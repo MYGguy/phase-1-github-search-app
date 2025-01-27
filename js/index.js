e77a777ca6802a51813738bb91c08cfdeff1d106
Moshe Goldstein <mygguy613@gmail.com>
2025-01-27 18:08:55 -0500

    add test user to dom with function

--- js/index.js
+++ js/index.js
@@ -4,7 +4,26 @@
 const form = document.querySelector('#github-form');
 const inputField = document.querySelector('#search');
 
+const userList = document.querySelector('#user-list');
+const reposList = document.querySelector('#repos-list');
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(event.target.children[0].value);
 })
+
+function addUserList(data) {
+    const div = document.createElement('div');
+    div.style = 'display: flex;';
+    div.innerHTML = `
+    <h1>${data.name}</h1>
+    <img src='${data.avatar}' style='width: 100px'/>
+    <a href='${data.link}'>Visit ${data.name}'s page</a>
+
+    `;
+
+    userList.appendChild(div);
+};
+const testUser = { name: 'octoman', avatar: 'https://avatars.githubusercontent.com/u/583231?v=4', link: 'https://github.com/octocat' };
+
+addUserList(testUser);

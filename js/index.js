950ca4f8ef28b890854f1c75dbfdd6847e6cc92c
Moshe Goldstein <mygguy613@gmail.com>
2025-01-27 18:27:13 -0500

    fetches first user and add to dom

--- js/index.js
+++ js/index.js
@@ -9,21 +9,38 @@ const reposList = document.querySelector('#repos-list');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event.target.children[0].value);
+    // console.log(event.target.children[0].value);
+
+    addUserList(event.target.children[0].value)
 })
 
-function addUserList(data) {
-    const div = document.createElement('div');
-    div.style = 'display: flex;';
-    div.innerHTML = `
-    <h1>${data.name}</h1>
-    <img src='${data.avatar}' style='width: 100px'/>
-    <a href='${data.link}'>Visit ${data.name}'s page</a>
+function addUserList(username) {
+    let data = {};
+    const configObject = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application / json',
+            'Accept': 'application/vnd.github.v3+json'
+        },
+        // body: JSON.stringify({
+        //     name: 'data.name',
+        //     avatar: 'data.avatar',
+        //     link: 'data.link',
+        // })
+    }
+    fetch(`https://api.github.com/search/users?q=${username}`, configObject)
+        .then(res => res.json())
+        .then(json => {
+            data = json.items;
 
-    `;
+            const div = document.createElement('div');
+            div.style = 'display: flex;';
+            div.innerHTML = `
+            <h1>${data[0].login}</h1>
+            <img src='${data[0].avatar_url}' style='width: 100px'/>
+            <a href='${data[0].html_url}'>Visit ${data[0].login}'s page</a>
+            `;
 
-    userList.appendChild(div);
+            userList.appendChild(div);
+        })
 };
-const testUser = { name: 'octoman', avatar: 'https://avatars.githubusercontent.com/u/583231?v=4', link: 'https://github.com/octocat' };
-
-addUserList(testUser);

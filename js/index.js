a60873f297df9625df58afdd5106a1986b4d9262
Moshe Goldstein <mygguy613@gmail.com>
2025-01-27 17:50:28 -0500

    get input value on submition

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 // user fetch: https://api.github.com/search/users?q=${username}
 // repo fetch: https://api.github.com/users/${username}/repos
 
+const form = document.querySelector('#github-form');
+const inputField = document.querySelector('#search');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target.children[0].value);
+})
